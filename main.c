c9b9d9e7987d68ed73335c12450ebb0ff3b47cc9
Ridone7 <ridwanalfarukhi@gmail.com>
2026-03-30 17:58:33 +0700

    penambahan menu

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    printf("=================================\n");
+    printf("ASISTEN KEUANGAN MAHASISWA\n");
+    printf("=================================\n");
+
+   printf("pilih menu: \n");
+
+    printf("1. Kalkulator diskon belanja\n");
+    printf("2. Kalkulator split bill\n");
+    printf("3. Kalkulator kalkulator bunga tabungan\n");
+    printf("4. Kalkulator kalkulator sisa uang jajan harian\n");
+    return 0;
+}
