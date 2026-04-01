3c731e898fb327f36c3f6dd59459e32aeb1adf19
nizaraffan <nizaraffang@gmail.com>
2026-04-01 17:30:31 +0700

    nizar-anggota2-splitbill

--- main.c
+++ main.c
@@ -1,18 +1,5 @@
 #include <stdio.h>
 
-int main() {
-    printf("=================================\n");
-    printf("ASISTEN KEUANGAN MAHASISWA\n");
-    printf("=================================\n");
-
-   printf("pilih menu: \n");
-
-    printf("1. Kalkulator diskon belanja\n");
-    printf("2. Kalkulator split bill\n");
-    printf("3. Kalkulator kalkulator bunga tabungan\n");
-    printf("4. Kalkulator kalkulator sisa uang jajan harian\n");
-    return 0;
-} 
 // nizarAnggota2
 void splitBill() {
     float totalTagihan;
