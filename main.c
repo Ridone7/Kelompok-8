c2b4872affecc54375ced94bee29ba3e5b1af43e
AhmdAzz <ahmadazizi59002@gmail.com>
2026-04-01 14:54:01 +0700

    menambahkan fungsi 4

--- main.c
+++ main.c
@@ -1,15 +1,43 @@
 #include <stdio.h>
 
+// fungsi 4
+void Kalkulator_Sisa_Uang_Jajan_Harian() {
+    float uSaku, pengeluaran, sisaU;
+
+    printf("Masukkan uang saku: ");
+    scanf("%f", &uSaku);
+
+    printf("Masukkan pengeluaran: ");
+    scanf("%f", &pengeluaran);
+
+    sisaU = uSaku - pengeluaran;
+
+    printf("Sisa uang saku = %.2f\n", sisaU);
+}
+
 int main() {
+    int pilihan;
+
     printf("=================================\n");
     printf("ASISTEN KEUANGAN MAHASISWA\n");
     printf("=================================\n");
 
-   printf("pilih menu: \n");
-
+    printf("Pilih menu:\n");
     printf("1. Kalkulator diskon belanja\n");
     printf("2. Kalkulator split bill\n");
-    printf("3. Kalkulator kalkulator bunga tabungan\n");
-    printf("4. Kalkulator kalkulator sisa uang jajan harian\n");
+    printf("3. Kalkulator bunga tabungan\n");
+    printf("4. Kalkulator sisa uang jajan harian\n");
+
+    printf("Masukkan pilihan: ");
+    scanf("%d", &pilihan);
+
+    switch(pilihan) {
+        case 4:
+            Kalkulator_Sisa_Uang_Jajan_Harian();
+            break;
+        default:
+            printf("Menu belum tersedia.\n");
+    }
+
     return 0;
 }
